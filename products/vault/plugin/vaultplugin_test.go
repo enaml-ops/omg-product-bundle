5fab80f30f6c831b199b5677649af532c285adc6
John Calabrese <jcalabrese@pivotal.io>
2016-07-23 19:10:21 -0400

    [#126918993] fixing hardcoded stemcell name bug. we now pull from flag value

--- products/vault/plugin/vaultplugin_test.go
+++ products/vault/plugin/vaultplugin_test.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/codegangsta/cli"
 	"github.com/enaml-ops/enaml"
-	"github.com/enaml-ops/pluginlib/util"
 	. "github.com/enaml-ops/omg-product-bundle/products/vault/plugin"
+	"github.com/enaml-ops/pluginlib/util"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -82,6 +82,36 @@ var _ = Describe("given vault Plugin", func() {
 			Ω(len(deployment.Releases)).Should(Equal(2))
 			Ω(len(deployment.Stemcells)).Should(Equal(1))
 			Ω(len(deployment.InstanceGroups)).Should(Equal(1))
+		})
+	})
+
+	Context("when calling GetProduct w/ a stemcell name flag ", func() {
+		var deployment *enaml.DeploymentManifest
+		var controlNetName = "private"
+		var controlDisk = "medium"
+		var controlVM = "medium"
+		var controlIP = "1.2.3.4"
+
+		BeforeEach(func() {
+			cloudConfigBytes, _ := ioutil.ReadFile("./fixtures/sample-aws.yml")
+			dmBytes := plgn.GetProduct([]string{
+				"appname",
+				"--network", controlNetName,
+				"--vm-type", controlVM,
+				"--disk-type", controlDisk,
+				"--ip", controlIP,
+				"--az", "z1",
+				"--stemcell-name", "blahname",
+				"--stemcell-url", "something",
+				"--stemcell-ver", "12.3.44",
+				"--stemcell-sha", "ilkjag09dhsg90ahsd09gsadg9",
+			}, cloudConfigBytes)
+			deployment = enaml.NewDeploymentManifest(dmBytes)
+		})
+		It("then we should have a properly configured stemcell definition in our deployment (os & alias from flag value)", func() {
+			Ω(len(deployment.Stemcells)).Should(Equal(1))
+			Ω(deployment.Stemcells[0].OS).Should(Equal("blahname"))
+			Ω(deployment.Stemcells[0].Alias).Should(Equal("blahname"))
 		})
 	})
 
