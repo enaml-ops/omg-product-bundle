78d1b2cfd4499399c911b5ce7a1402f05d256df6
John Calabrese <jcalabrese@pivotal.io>
2016-10-25 17:09:25 -0400

    [#133040925] oss-cf plugin enaml generate from releases

---
+++ products/oss_cf/enaml-gen/gorouter/gorouterjob_marshal_test.go
@@ -0,0 +1,32 @@
+package gorouter_test
+
+import (
+	"github.com/enaml-ops/omg-product-bundle/products/oss_cf/enaml-gen/gorouter"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	yaml "gopkg.in/yaml.v2"
+)
+
+const controlYAML = `routing-api:
+  port: 1234
+  auth_disabled: true
+routing_api:
+  enabled: true
+`
+
+var _ = Describe("given GorouterJob", func() {
+	Context("when marshalling routing API", func() {
+		It("produces the correct YAML", func() {
+			j := gorouter.GorouterJob{
+				RoutingApi: &gorouter.RoutingApi{
+					Port:         1234,
+					AuthDisabled: true,
+					Enabled:      true,
+				},
+			}
+			b, err := yaml.Marshal(&j)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(b).Should(MatchYAML(controlYAML))
+		})
+	})
+})
